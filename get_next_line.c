96d21fd9b2e64a2616e0cb8a3f0f32995563d19f
Timajitsu <65711404+Timajitsu@users.noreply.github.com>
2021-12-28 16:48:03 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taliza <taliza@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 15:04:08 by taliza            #+#    #+#             */
+/*   Updated: 2021/12/13 15:04:17 by taliza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <unistd.h>
 
-char *ft_read_to_left_str(int fd, char *left_str)
+char	*ft_read_to_left_str(int fd, char *left_str)
 {
-	char 	*buff;
-	int 	rd_bytes;
+	char	*buff;
+	int		rd_bytes;
 
 	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if(!buff)
+	if (!buff)
 		return (NULL);
 	rd_bytes = 1;
 	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
@@ -27,15 +39,15 @@ char *ft_read_to_left_str(int fd, char *left_str)
 
 char	*get_next_line(int fd)
 {
-	char	*line;
+	char		*line;
 	static char	*left_str;
 
-	if(fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	left_str = ft_read_to_left_str(fd, left_str);
-	if(!left_str)
-		return(NULL);
+	if (!left_str)
+		return (NULL);
 	line = ft_get_line(left_str);
 	left_str = ft_new_left_str(left_str);
-	return(line);
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taliza <taliza@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 15:04:12 by taliza            #+#    #+#             */
+/*   Updated: 2021/12/13 15:04:14 by taliza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -12,7 +24,7 @@ char	*ft_read_to_left_str(int fd, char *left_str);
 char	*ft_strchr(char *s, int c);
 char	*ft_strjoin(char *let_str, char *buff);
 size_t	ft_strlen(char *s);
-char 	*ft_get_line(char *lest_str);
-char 	*ft_new_left_str(char *left_str);
+char	*ft_get_line(char *lest_str);
+char	*ft_new_left_str(char *left_str);
 
 #endif //GET_NEXT_LINE_H
